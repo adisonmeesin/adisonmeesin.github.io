ac1d474b01a10e1d08d54484a1a470c04651a114
adisonmeesin <48058374+adisonmeesin@users.noreply.github.com>
2021-08-08 21:45:16 +0700

    change to php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+    Welcome to Smart Home!!
+
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+date_default_timezone_set("Asia/Bangkok");
+echo "The time is " . date("h:i:sa");
+?>
+
+</body>
+</html>
