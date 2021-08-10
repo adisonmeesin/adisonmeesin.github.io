31d17c78f4f0473ffa9b5401766d9d9fc74eb67c
adisonmeesin <48058374+adisonmeesin@users.noreply.github.com>
2021-08-10 09:51:59 +0700

    Loop in item

--- js/chunk-2d22d746.9f343acb.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about text-center"},[n("h1",[t._v("This is an about page")]),n("v-btn",{attrs:{depressed:"",color:"primary",to:"/"}},[t._v(" Go to Home Page ")])],1)},s=[],o=n("2877"),r=n("6544"),c=n.n(r),u=n("8336"),i={},l=Object(o["a"])(i,a,s,!1,null,null,null);e["default"]=l.exports;c()(l,{VBtn:u["a"]})}}]);
-//# sourceMappingURL=chunk-2d22d746.9f343acb.js.map
